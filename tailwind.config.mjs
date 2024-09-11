80cd30e75956b7f1630416d1aeb0d1317dcff363
Ethan Hassett <ethan@hassett.io>
2024-09-11 15:31:33 +0000

    feat: add initial styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			screens: {
-				'3xl': '1792px',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "oxford-blue": "#001D3D",
+        "mikado-yellow": "#FFC300",
+      },
+      screens: {
+        "3xl": "1792px",
+      },
+    },
+  },
+  plugins: [],
+};
