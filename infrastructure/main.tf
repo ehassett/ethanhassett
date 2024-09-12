9966a29485979d3e6b7de45097844ffa2b847276
Ethan Hassett <ethan@hassett.io>
2024-09-12 17:11:47 -0400

    fix: update public access to bucket (#8)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,34 +3,35 @@ locals {
 }
 
 resource "google_storage_bucket" "this" {
-  name     = "ethanhassett.com"
-  location = "US"
-
-  force_destroy = false
+  name          = "ethanhassett.com"
+  location      = "US"
   storage_class = "STANDARD"
 
-  #checkov:skip=CKV_GCP_29:no need for uniform_bucket_level_access at this time
+  force_destroy               = false
+  uniform_bucket_level_access = true
+
   #checkov:skip=CKV_GCP_62:no need for access logs at this time
   #checkov:skip=CKV_GCP_78:no need for versioning at this time
   #checkov:skip=CKV_GCP_114:no need for public_access_prevention at this time
-}
-
-resource "google_storage_bucket_access_control" "public_read_access" {
-  bucket = google_storage_bucket.this.name
-  role   = "READER"
-  entity = "allUsers"
-}
-
-resource "google_storage_default_object_access_control" "public_read_access" {
-  bucket = google_storage_bucket.this.name
-  role   = "READER"
-  entity = "allUsers"
 }
 
 resource "google_storage_bucket_object" "img" {
   for_each = fileset("${local.objects_path}/img", "*")
 
   bucket = google_storage_bucket.this.name
-  name   = "img/${each.key}"
+  name   = "public/img/${each.key}"
   source = "${local.objects_path}/img/${each.key}"
+}
+
+resource "google_storage_managed_folder" "public" {
+  bucket        = google_storage_bucket.this.name
+  name          = "public/"
+  force_destroy = false
+}
+
+resource "google_storage_managed_folder_iam_binding" "public" {
+  bucket         = google_storage_managed_folder.public.bucket
+  managed_folder = google_storage_managed_folder.public.name
+  role           = "roles/storage.objectViewer"
+  members        = ["allUsers"]
 }
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,4 +1,4 @@
 output "img_urls" {
-  description = "Media URLs for objects in img folder."
-  value       = { for obj in google_storage_bucket_object.img : obj.name => obj.media_link }
+  description = "URLs for objects in public/img."
+  value       = { for obj in google_storage_bucket_object.img : obj.name => obj.self_link }
 }
