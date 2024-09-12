9e7a491c1195bb9ecb0cd1ee6f9d5f94b9273955
Ethan Hassett <ethan@hassett.io>
2024-09-12 17:45:15 -0400

    feat: add CDN resources (#10)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,7 @@ locals {
   objects_path = "${path.module}/objects"
 }
 
+# Backend Storage Bucket
 resource "google_storage_bucket" "this" {
   name          = "ethanhassett.com"
   location      = "US"
@@ -32,6 +33,45 @@ resource "google_storage_managed_folder" "public" {
 resource "google_storage_managed_folder_iam_binding" "public" {
   bucket         = google_storage_managed_folder.public.bucket
   managed_folder = google_storage_managed_folder.public.name
-  role           = "roles/storage.objects.get"
+  role           = "roles/storage.objectViewer"
   members        = ["allUsers"]
+}
+
+# CDN
+resource "google_compute_global_address" "cdn" {
+  name = "ethanhassett-com-cdn"
+}
+
+resource "google_compute_backend_bucket" "cdn" {
+  name        = "ethanhassett-com-backend-bucket"
+  description = "Backend bucket for ethanhassett.com"
+  bucket_name = google_storage_bucket.this.name
+  enable_cdn  = true
+  cdn_policy {
+    cache_mode        = "CACHE_ALL_STATIC"
+    client_ttl        = 3600
+    default_ttl       = 3600
+    max_ttl           = 86400
+    negative_caching  = true
+    serve_while_stale = 86400
+  }
+}
+
+resource "google_compute_url_map" "cdn" {
+  name            = "http-lb"
+  default_service = google_compute_backend_bucket.cdn.id
+}
+
+resource "google_compute_target_http_proxy" "cdn" {
+  name    = "http-lb-proxy"
+  url_map = google_compute_url_map.cdn.id
+}
+
+resource "google_compute_global_forwarding_rule" "cdn" {
+  name                  = "http-lb-forwarding-rule"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.cdn.id
+  ip_address            = google_compute_global_address.cdn.id
 }
