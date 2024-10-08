672cb28c967f2c051067d3bdff32a6e1ae98c298
Ethan Hassett <ethan@hassett.io>
2024-10-07 22:33:14 -0400

    feat: add description, sitemap, robots.txt, and update dependencies (#47)

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -5,9 +5,12 @@ import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://ethanhassett.com",
+  integrations: [tailwind(), sitemap()],
   output: "server",
 
   adapter: node({
