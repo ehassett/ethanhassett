1910f7c96350dd3cc2cc9d37c4df909bbe377c2a
Ethan Hassett <ethan@hassett.io>
2025-03-04 22:48:57 +0000

    feat: upgrade to Tailwind v4

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
-
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ethanhassett.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [sitemap()],
   output: "server",
 
   adapter: node({
     mode: "standalone",
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
