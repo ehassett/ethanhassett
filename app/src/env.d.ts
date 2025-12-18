bfd84b1f42bf1a71f8bb2a1fa245acb782f67744
Ethan Hassett <ethan@hassett.io>
2025-12-17 20:12:52 -0500

    feat: update dependencies, use middleware, switch to Mailgun package (#173)
    * ci: update Actions dependencies
    
    * chore(deps): upgrade node and package versions
    
    * fix: update wrangler config
    
    * fix: use mailgun package for sending email (#171)
    
    * ci: add check for node version in wrangler config and .tool-versions
    
    * feat: add middleware and proper handling of server errors (#172)

--- app/src/env.d.ts
+++ app/src/env.d.ts
@@ -1,5 +1,21 @@
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 
 declare namespace App {
-  interface Locals extends Runtime {}
+  interface Locals extends Runtime {
+    contact: {
+      name: {
+        value: string;
+        error: string;
+      };
+      email: {
+        value: string;
+        error: string;
+      };
+      message: {
+        value: string;
+        error: string;
+      };
+      serverError: boolean;
+    };
+  }
 }
